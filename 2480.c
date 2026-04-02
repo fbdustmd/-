0ab55df7f35a0bd2a8a17a15acbe290d5e70d65a
fbdustmd <jonathan7ryu@gmail.com>
2026-04-02 15:17:59 +0900

    Add files via upload

--- 2480.c
+++ 2480.c
@@ -1,52 +1,32 @@
 #include <stdio.h>
-
 int main()
 {
-    int a, b, c, d;
-    scanf("%d %d %d", &a, &b, &c);
-    if (a == b && b == c) // 셋다 같을경우 
-    {
-        d = 10000 + a * 1000;
-        printf("%d", d);
-    }
-    if (a == b && b != c) // a==b
-    {
-        printf("%d", 1000 + b * 100);
-    }
-    if (c == b && b != a) // b==c
-    {
-        printf("%d", 1000 + b * 100);
-    }
-    if (a == c && b != c) // a == c
-    {
-        printf("%d", 1000 + 100 * a);
-    }
-    else
-    {
-        if (a > b && b > c)
-        {
-            printf("%d", a * 100);
-        }
-        if (a > c && c > b)
-        {
-            printf("%d", a * 100);
-        }
-        if (b > a && a > c)
-        {
-            printf("%d", b * 100);
-        }
-        if (b > c && c > a)
-        {
-            printf("%d", b * 100);
-        }
-        if (c > a && a > b)
-        {
-            printf("%d", c * 100);
-        }
-        if (c > b && b > a)
-        {
-            printf("%d", c * 100);
-        }
-    }
-    return 0;
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a == b && b == c) { // 셋다 같은경우 
+		printf("%d", 10000 + 1000 * a);
+		return 0; //  이때 return 0; 을 안넣으면 음 문제생길거같아서 해놓음 
+	}
+	else if (a == b || b == c || a == c) { // 둘만 같은경우 
+		if (a == b) {
+			printf("%d", 1000 + a * 100);
+		}
+		else if (b == c) {
+			printf("%d", 1000 + b * 100);
+		}
+		else {
+			printf("%d", 1000 + c * 100);
+		}
+	}
+	else { // 이떄부터는 무조건 셋다 다름 
+		if (a > b && a > c) {
+			printf("%d", a * 100);
+		}
+		else if (b > a && b > c) {
+			printf("%d", b * 100);
+		}
+		else {
+			printf("%d", c * 100);
+		}
+	}
 }
