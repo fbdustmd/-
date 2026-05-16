1affe25ec44132c00a4e590cb559eeebfbb57f75
fbdustmd <jonathan7ryu@gmail.com>
2026-05-17 06:09:33 +0900

    Update Wow.c

--- Wow.c
+++ Wow.c
@@ -51,46 +51,33 @@ int main(int argc, char* argv[]) {
             printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
             break;
         }
-
         memcpy(eth, packet, header->caplen);
-
         if (header->caplen < 34) goto skip;
-
         if (eth[23] != 6) {
             printf("not tcp \n\n");
             goto skip;
         }
-
         unsigned char x = eth[14];
         int alal = 8;
         sum = eth[14] & 0x0F;
-        int tcp_start = 14 + sum * 4;
-
+        int tcp_start = 14 + sum * 4;   
         printf("==========Ethernet Header==========\nDst Mac : ");
-
         for (int i = 0; i < 12; i++) {
             printf("%02x", eth[i]);
-
             if (i != 5 && i != 11) printf(":");
             if (i == 5) printf("\nSrc Mac : ");
         }
-
         printf("\n");
         printf("==========IP Header==========\n");
         printf("Src IP : ");
-
         for (int i = 26; i < 34; i++) {
             printf("%d", eth[i]);
-
             if (i != 29 && i != 33) printf(".");
             if (i == 29) printf("\nDst IP : ");
         }
-
         printf("\n==========TCP Header==========\nSrc Port : ");
-
         int src_port = (eth[tcp_start] << 8) | eth[tcp_start + 1];
         int dst_port = (eth[tcp_start + 2] << 8) | eth[tcp_start + 3];
-
         printf("%d\n", src_port);
         printf("Dst Port : ");
         printf("%d", dst_port);
